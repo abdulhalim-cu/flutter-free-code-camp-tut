42bbeef8c9f0f21270c294e4679278854e32c5d9
Abdul Halim <abdul.halim@bs-23.net>
2020-10-24 21:30:26 +0600

    Update TextSection widget
    TextSection widget takes the title and body arguments.
    Title and body wrapped with padding.

--- lib/screen/location_detail/local_detail.dart
+++ lib/screen/location_detail/local_detail.dart
@@ -15,9 +15,9 @@ class LocationDetail extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           ImageBanner("assets/images/kiyomizu-dera.jpg"),
-          TextSection(Colors.red),
-          TextSection(Colors.green),
-          TextSection(Colors.blue),
+          TextSection("summary", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
+          TextSection("summary", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
+          TextSection("summary", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
         ],
       ),
     );
--- lib/screen/location_detail/text_section.dart
+++ lib/screen/location_detail/text_section.dart
@@ -2,20 +2,40 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TextSection extends StatelessWidget {
-  final Color _color;
+  // final Color _color;
 
   /*TextSection(Color color) {
     this._color = color;
   }*/
-  TextSection(this._color); // constructor with positional parameter
+  // TextSection(this._color); // constructor with positional parameter
+
+  final String _title, _body;
+  static const double _hPad = 16.0;
+
+  TextSection(this._title, this._body);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    /*return Container(
       decoration: BoxDecoration(
         color: _color,
       ),
       child: Text('box-box-box'),
+    );*/
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          padding: const EdgeInsets.fromLTRB(_hPad, 30.0, _hPad, 4.0),
+          child: Text(_title),
+        ),
+        Container(
+          padding: const EdgeInsets.fromLTRB(_hPad, 10.0, _hPad, _hPad),
+          child: Text(_body),
+        ),
+      ],
     );
   }
 }
