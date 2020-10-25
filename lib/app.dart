e00d75ad4fd3d65e9de397f25e3edde443574822
Abdul Halim <abdul.halim@bs-23.net>
2020-10-25 07:18:35 +0600

    Style widget added
    We have added style widget in the material widget section to implement
    all over the project. Then applied the textTheme to our text section.
    We created different text style such as app bar text style, body text
    style and so on.

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'screen/location_detail/local_detail.dart';
+import 'style.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: LocationDetail(),
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          textTheme: TextTheme(headline6: AppBarTextStyle),
+        ),
+        textTheme: TextTheme(
+          headline6: TitleTextStyle,
+          bodyText1:  BodyText1Style,
+        )
+      ),
     );
   }
 }
