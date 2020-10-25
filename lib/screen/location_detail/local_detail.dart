d0974dbe2aa23eee432877e0266e4793e3c3e0c3
Abdul Halim <abdul.halim@bs-23.net>
2020-10-25 08:08:55 +0600

    location model implementation
    Now we implemented location model to our location detail widget
    we called fetchAll() method to get all locations. As we have
    only one location for now we get its first location. Then,
    location.name inserted into the title section, imagePath into the
    ImageBanner widget and textSection of location cascaded with the children.
    
    textSection() get the location and converted it to the list of widget.

--- lib/screen/location_detail/local_detail.dart
+++ lib/screen/location_detail/local_detail.dart
@@ -2,24 +2,30 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'text_section.dart';
 import 'image_banner.dart';
+import 'package:tourism/models/location.dart';
 
 class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final locations = Location.fetchAll();
+    final location = locations.first;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello'),
+        title: Text(location.name),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          ImageBanner("assets/images/kiyomizu-dera.jpg"),
-          TextSection("summary", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
-          TextSection("summary", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
-          TextSection("summary", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
-        ],
+          ImageBanner(location.imagePath),
+        ]..addAll(textSection(location)),
       ),
     );
+  }
+
+  List<Widget> textSection(Location location) {
+    return location.facts.map((fact) => TextSection(fact.title, fact.text)).toList();
   }
 }
